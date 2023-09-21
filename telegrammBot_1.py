7f0e541e3d6928924b082fb7fe620f5835b032a5
garlik2003 <142458070+garlik2003@users.noreply.github.com>
2023-09-21 19:34:44 +0300

    Add files via upload

---
+++ telegrammBot_1.py
@@ -0,0 +1,14 @@
+# токен telegram-бота
+TOKEN = "Token for telegram"
+
+# токен для API
+header = {
+  "apikey": "api key for currencies"
+}
+
+# словарь доступных пользователю валют
+keys = {"рубль": "RUB",
+        "евро": "EUR",
+        "доллар": "USD",
+        "авс.доллар": 'AUD',
+        }
